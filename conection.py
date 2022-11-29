c971b49fc3fa603a79240503cf76686215026fd0
Kevin Sama 22 <113372511+ksama22@users.noreply.github.com>
2022-11-29 20:12:57 +0100

    Conection to database (#6)
    * conexio

---
+++ conection.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+#Crea una conexio a la base de dades.
+def conexio():
+    connection = None
+    try:
+        # Si funciona conecta
+        connection = psycopg2.connect(user="",
+                                      password="",
+                                      host="localhost",
+                                      port="5432",
+                                      database="bida")
+    except:
+        #Si falla mostra 'Problema de conexio'
+        print("Problema en la conexio")
+    return connection
